c7ffe9db3ab4ac5923ac4c087a42663980db37aa
xw0078 <xw0078@vt.edu>
2020-05-25 18:19:03 -0400

    update

--- account_revive.py
+++ account_revive.py
@@ -10,39 +10,46 @@ sys.path.append(os.getcwd())
 from crawl_user_timeline.sina.settings import LOCAL_MONGO_HOST, LOCAL_MONGO_PORT, DB_NAME, PROXY_BASEURL
 import time
 from build_account import WeiboLogin
+from apscheduler.schedulers.blocking import BlockingScheduler
 
-myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-mydb = myclient[DB_NAME]
-mycol = mydb["account"]
 
-# find error account
-myquery = { 
-    "$or": [
-        {"status": "error"},
-        {"status": {"$exists": False}}
-    ]
-}
-mydoc = mycol.find(myquery)
+def account_revive():
+    myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+    mydb = myclient[DB_NAME]
+    mycol = mydb["account"]
 
-# revive account
-if mydoc.count():
-    for x in mydoc:
-        username = x["_id"]
-        password = x["password"]
-        try:
-            print('Reviving: '+str(username))
-            cookie_str = WeiboLogin(username, password).run()
-            print('获取cookie成功')
-            print('Cookie:')
-            print(cookie_str)
-        except Exception as e:
-            print(e)
-            continue
+    # find error account
+    myquery = { 
+        "$or": [
+            {"status": "error"},
+            {"status": {"$exists": False}}
+        ]
+    }
+    mydoc = mycol.find(myquery)
 
-        try:
-            mycol.insert(
-                {"_id": username, "password": password, "cookie": cookie_str, "status": "success"})
-        except DuplicateKeyError as e:
-            mycol.find_one_and_update({'_id': username}, {'$set': {'cookie': cookie_str, "status": "success"}})
-else:
-    print("No error account found")
+    # revive account
+    if mydoc.count():
+        for x in mydoc:
+            username = x["_id"]
+            password = x["password"]
+            try:
+                print('Reviving: '+str(username))
+                cookie_str = WeiboLogin(username, password).run()
+                print('获取cookie成功')
+                print('Cookie:')
+                print(cookie_str)
+            except Exception as e:
+                print(e)
+                continue
+
+            try:
+                mycol.insert(
+                    {"_id": username, "password": password, "cookie": cookie_str, "status": "success"})
+            except DuplicateKeyError as e:
+                mycol.find_one_and_update({'_id': username}, {'$set': {'cookie': cookie_str, "status": "success"}})
+    else:
+        print("No error account found")
+
+scheduler = BlockingScheduler()
+scheduler.add_job(account_revive, 'interval', hours=1)
+scheduler.start()
